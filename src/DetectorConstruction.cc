b379f1b8011f6ec506764058efcc5686528df79f
Guy Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-09-26 17:36:41 +0100

    adding diamond refractive index

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -35,10 +35,15 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 	G4double boxSize = 0.5*m;
 	G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2};
 	G4double rindexWorld[2] = {1.0, 1.0};
+	G4double rindexDiamond[2] = {2.4168, 2.4168};
 	
 	G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
 	mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
 	worldMat->SetMaterialPropertiesTable(mptWorld);
+	
+	G4MaterialPropertiesTable *mptDiamond = new G4MaterialPropertiesTable();
+	mptDiamond->AddProperty("RINDEX", energy, rindexDiamond, 2);
+	diamond->SetMaterialPropertiesTable(mptDiamond);
 	
 	G4Box *solidWorld = new G4Box("solidWorld", boxSize, boxSize, boxSize);
 	G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
