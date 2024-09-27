7cadd93a4ebfce8e8139250631b34d54dd0b7d91
Guy Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-09-27 13:57:38 +0100

    using charged geantino as particle with 21Na mass

--- src/generator.cc
+++ src/generator.cc
@@ -3,12 +3,12 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
 	G4int n_particle = 1;
-	fParticleGun  = new G4ParticleGun(n_particle);
+	fParticleGun = new G4ParticleGun(n_particle);
 	
-	fParticleGun->SetParticleEnergy(0*eV);
-	fParticleGun->SetParticlePosition(G4ThreeVector(0.,0.,0.));
-	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(1.,0.,0.));
-	
+	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+	G4ParticleDefinition *particle = particleTable->FindParticle("chargedgeantino");
+
+	fParticleGun->SetParticleDefinition(particle);
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
@@ -18,14 +18,24 @@ MyPrimaryGenerator::~MyPrimaryGenerator()
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
-	if (fParticleGun->GetParticleDefinition() == G4Geantino::Geantino()) {  
-	    G4int Z = 10, A = 24;
-	    G4double ionCharge   = 1.*eplus;
-	    G4double excitEnergy = 0.*keV;
-	    
-	    G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z,A,excitEnergy);
-	    fParticleGun->SetParticleDefinition(ion);
-	    fParticleGun->SetParticleCharge(ionCharge);
+	G4ParticleDefinition* particle = fParticleGun->GetParticleDefinition();
+
+	if (particle == G4ChargedGeantino::ChargedGeantino()) 
+	{  
+		G4int Z = 11, A = 21;
+		G4double ionCharge   = 11.*eplus;
+		G4double excitEnergy = 0.*keV;
+		G4ThreeVector pos(0.,0.,-0.25*m);
+		G4ThreeVector mom(0.,0.,1.);
+		G4double energy = 5.*MeV;
+		
+		G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z,A,excitEnergy);
+		
+		fParticleGun->SetParticleEnergy(energy);
+		fParticleGun->SetParticlePosition(pos);
+		fParticleGun->SetParticleMomentumDirection(mom);
+		fParticleGun->SetParticleDefinition(ion);
+		fParticleGun->SetParticleCharge(ionCharge);
   	}      
   	fParticleGun->GeneratePrimaryVertex(anEvent);
 }
