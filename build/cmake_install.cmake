71f943531e82a7d5a56eeae5082124237099f9a1
Guy Carter-Richardson <guy.carter-richardson@outlook.com>
2024-10-02 21:49:12 +0100

    adding readme files to both source and results directories detailing actons for tomorrow

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/diamondDetector
+# Install script for directory: /home/guy/software/geant4/geant4-v11.2.1-mt-gdml-install/projects/diamondDetector
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/guy/software/geant4/geant4-v11.2.2-gdml-mt-install/projects/diamondDetector/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/guy/software/geant4/geant4-v11.2.1-mt-gdml-install/projects/diamondDetector/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
