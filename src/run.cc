7184c045253ca9fe26501c12c736253e8bd19aad
Guy Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-11-11 11:14:03 +0000

    update to allow for varying radial distribution of GPS

--- src/run.cc
+++ src/run.cc
@@ -66,9 +66,10 @@ void MyRunAction::BeginOfRunAction(const G4Run* run)
 	
 	//different root file name options
 	//man->OpenFile("RunID:"+strRunID1.str()+".root");
+	man->OpenFile("RunID:"+strRunID.str()+".root");
 	//man->OpenFile("75cmIDpos"+DDPosStr.str()+"cmDDpos_"+DDSizeStr.str()+"mmDD_ID"+IDStr+"DD"+DDStr+strRunID.str()+"deg.root");
 	//man->OpenFile("75cmIDpos"+DDPosStr.str()+"cmDDpos_"+DDSizeStr.str()+"mmDD_ID"+IDStr+"DD"+DDStr+"4deg.root");
-	man->OpenFile("100cm_20mm"+strRunID1.str()+"deg.root");
+	//man->OpenFile("100cm_20mm"+strRunID1.str()+"deg.root");
 	
 	//finding time to define .root file names
 	time_t now = time(0);
